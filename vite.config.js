bcd282cdfc5b0cd781b0555982c97134b4c91756
El-Pugi <Simonen.pauli@hotmail.com>
2024-04-22 14:16:26 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { Environment } from 'ag-grid-community'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/carapp',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
